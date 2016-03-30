e1aa3fc305f384bfecf026762583607c2027226e
Leif Jurvetson <leijurv@gmail.com>
2016-03-30 14:54:09 -0700

    I literally can't

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+"fmt"
+"net"
+"bufio"
+)
 
 func main() {
     fmt.Printf("Hello, world.\n")
+    listen()
 }
+
+func listen(){
+	ln, err := net.Listen("tcp", ":8080")
+if err != nil {
+	// handle error
+}
+for {
+	conn, err := ln.Accept()
+	if err != nil {
+		// handle error
+	}
+	go func(){
+		fmt.Printf("CONNECTION BABE")
+		status,_:=bufio.NewReader(conn).ReadString('\n')
+		
+		fmt.Printf(status)
+		}()
+}
+}
+
