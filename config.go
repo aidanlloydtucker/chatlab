fa22cd1a9cbdb235ebe619495c80e2d6136f2635
Aidan Lloyd-Tucker <aidanlloydtucker@gmail.com>
2016-03-31 12:51:25 -0700

    config works now

--- config.go
+++ config.go
@@ -1,36 +1,61 @@
 package main
 
-import "os"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
 var config *Config
 
 type Config struct {
-	username   string
-	privateKey string
-	passphrase string
+	Username   string
+	PrivateKey string
+	Passphrase string
 }
 
 func makeConfig() (*Config, error) {
 	var newConfig = &Config{
-		username:   "user",
-		privateKey: "",
-		passphrase: "",
+		Username:   "user",
+		PrivateKey: "./key.key",
+		Passphrase: "./pass.key",
 	}
 	return newConfig, nil
 }
 
 func loadConfig() error {
-	configFile, err := os.Open("./config.json")
-	if err != nil {
-		return err
-	}
-	defer configFile.Close()
-	if _, err := configFile.Stat(); !os.IsNotExist(err) {
-		newConfig, err := makeConfig()
+	if _, err := os.Stat("./config.json"); os.IsNotExist(err) {
+		var newConfig *Config
+		newConfig, err = makeConfig()
+		if err != nil {
+			return err
+		}
 		config = newConfig
+	} else {
+		configFile, err := os.Open("./config.json")
+		if err != nil {
+			return err
+		}
+		defer configFile.Close()
+		byteArrFile, err := ioutil.ReadAll(configFile)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(byteArrFile, &config)
 		if err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func saveConfig() error {
+	file, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(file))
+	err = ioutil.WriteFile("./config.json", file, 0777)
+	return err
 }
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
 	// Passphrase for private key
 	/*var passprase, err = ioutil.ReadFile("./pass.key")
@@ -7,6 +14,28 @@ func main() {
 		panic(err)
 	}*/
 
+	err := loadConfig()
+	if err != nil {
+		panic(err)
+	}
+
 	go printAll(outputChannel)
-	listen()
+	go listen()
+
+	// Exit capture
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		_ = <-sigs
+		err := saveConfig()
+		if err != nil {
+			panic(err)
+		}
+		done <- true
+	}()
+	<-done
+	fmt.Println("Safe Exited")
 }
